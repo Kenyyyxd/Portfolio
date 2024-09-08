b1099f2cc463eff72fe973878c0456421a60c0e4
Kenyyy <k.acuna1398@gmail.com>
2024-09-08 16:26:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function rotateAngledown(){
    
 }
 
+let darkmode = localStorage.getItem("light-mode");
+const themeSwitch = document.getElementById("theme-switch");
+
+const enableDarkmode = () => {
+  document.body.classList.add("light-mode");
+  localStorage.setItem("light-mode", "active");
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove("light-mode");
+  localStorage.setItem("light-mode", null);
+};
+
+if (darkmode === "active") enableDarkmode();
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("light-mode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
